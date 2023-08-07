3ce06b43cef195dd77058b1353aac123eb5fa00e
Steven Deobald <steven@deobald.ca>
2023-08-07 13:53:56 -0600

    add a little todo-checker script

---
+++ bin/list-todos.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+# **/*.rb is easier, but MacOS Bash doesn't include globstar by default
+# and we don't want to assume the user executing this script is on zsh
+grep -inr --include \*.rb "TODO:" .
