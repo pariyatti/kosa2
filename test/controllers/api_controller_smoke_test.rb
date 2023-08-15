c4c466aa3c722f8a2d622635e6d479426088ff1b
Steven Deobald <steven@deobald.ca>
2023-08-15 00:21:59 -0600

    fix: reorder smoke test

--- test/controllers/api_controller_smoke_test.rb
+++ test/controllers/api_controller_smoke_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 using RefinedHash
 
 class ApiControllerSmokeTest < ActionDispatch::IntegrationTest
+
   smoke_test "should get today" do
 
     LoopedPaliWord.ingest_all
@@ -18,10 +19,12 @@ class ApiControllerSmokeTest < ActionDispatch::IntegrationTest
     json_body = JSON.parse(response.body)
     # puts json_body.inspect
 
-    pubbe = JSON.parse(file_fixture("pali_word_pubbe.json").read)
-    assert_json pubbe, json_body[0]
     naekarama = JSON.parse(file_fixture("doha_naekarama.json").read)
-    assert_json naekarama, json_body[1]
+    assert_json naekarama, json_body[0]
+
+    pubbe = JSON.parse(file_fixture("pali_word_pubbe.json").read)
+    assert_json pubbe, json_body[1]
+
     appam = JSON.parse(file_fixture("words_of_buddha_appam.json").read)
     assert_json appam, json_body[2]
   end
