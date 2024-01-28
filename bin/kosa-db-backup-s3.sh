96e0dce594d95d3f886dcbc33efe665cf5cd436d
Tanmay Balwa <tanmay.balwa@gmail.com>
2024-01-28 22:37:30 +0900

    Change backup path to /tmp/

--- bin/kosa-db-backup-s3.sh
+++ bin/kosa-db-backup-s3.sh
@@ -18,17 +18,17 @@ container_id=$(docker ps | grep $service_name | awk '{print $1}')
 $docker_bin exec $container_id pg_dump -U kosa2 -f /tmp/$backup_filename $database_name
 
 # copy file inside contaienr to host
-$docker_bin cp $container_id:/tmp/$backup_filename .
+$docker_bin cp $container_id:/tmp/$backup_filename /tmp/$backup_filename
 
 # remove file in container
 $docker_bin exec $container_id rm /tmp/$backup_filename
 
 # compress
-gzip $backup_filename
+gzip /tmp/$backup_filename
 
 # upload to s3
-$aws_bin s3 cp $backup_filename_zipped $s3_location
+$aws_bin s3 cp /tmp/$backup_filename_zipped $s3_location
 
-rm $backup_filename_zipped
+rm /tmp/$backup_filename_zipped
 
 echo "Backup completed. Filename: $backup_filename_zipped"
