e6c2e00769cc005cda6bc8862adaa70303811a0e
Tanmay Balwa <tanmay.balwa@gmail.com>
2024-02-04 00:01:04 +0900

    Add production efs

---
+++ ops/terraform/kosa2-efs/production/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.5.7"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+  backend "s3" {
+    bucket = "pariyatti-tf-state-bucket"
+    key    = "kosa2/efs-volume/production/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
