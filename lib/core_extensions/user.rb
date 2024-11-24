f41c87996675ef14490a76112518594c1b0763f0
Steven Deobald <steven@deobald.ca>
2024-11-23 20:35:31 -0400

    split download/sync into two parts

--- lib/core_extensions/user.rb
+++ lib/core_extensions/user.rb
@@ -10,6 +10,7 @@ module CoreExtensions
         while next_page
           puts "Downloading next page: #{next_page}"
           page = get(next_page)
+          puts page.keys
           page_videos = page['data'].map {|h| to_video(h) }
           videos.concat(page_videos)
           next_page = prefix_endpoint(page['paging']['next'])
@@ -26,26 +27,26 @@ module CoreExtensions
 
       def to_video(h)
         v = Video.new(
-          uri:              h['uri'],
-          name:             h['name'],
-          description:      h['description'],
-          link:             h['link'],
-          player_embed_url: h['player_embed_url'],
-          embed_html:       h['embed_html'],
-          width:            h['width'],
-          height:           h['height'],
-          language:         h['language'],
-          duration:         h['duration'],
-          created_time:     h['created_time'],
-          modified_time:    h['modified_time'],
-          release_time:     h['release_time'],
-          privacy_view:     h['privacy']['view'],
-          privacy_embed:    h['privacy']['embed'],
-          privacy_download: h['privacy']['download'],
+          uri:               h['uri'],
+          name:              h['name'],
+          description:       h['description'],
+          link:              h['link'],
+          player_embed_url:  h['player_embed_url'],
+          embed_html:        h['embed_html'],
+          width:             h['width'],
+          height:            h['height'],
+          language:          h['language'],
+          duration:          h['duration'],
+          created_time:      h['created_time'],
+          modified_time:     h['modified_time'],
+          release_time:      h['release_time'],
+          privacy_view:      h['privacy']['view'],
+          privacy_embed:     h['privacy']['embed'],
+          privacy_download:  h['privacy']['download'],
           picture_base_link: h['pictures']['base_link'],
-          tags:             h['tags'].map { |t| t['name'] }, #.join(','),
-          play_stats:       h['stats']['plays'],
-          status:           h['status']
+          tags:              h['tags'].map { |t| t['name'] }, #.join(','),
+          play_stats:        h['stats']['plays'],
+          status:            h['status']
         )
         v.validate!
         v
