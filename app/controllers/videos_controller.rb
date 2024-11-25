166445c727d7639ea3894be36e6cd182551f8d5b
Steven Deobald <steven@deobald.ca>
2024-11-25 16:22:07 -0400

    simple little search API

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,4 +2,12 @@ class VideosController < ApplicationController
   def index
     render json: Video.order(release_time: :desc).all
   end
+
+  def search
+    where = <<~SQL
+      lower(concat(description, name)) 
+      LIKE lower(concat('%', ?, '%'))
+    SQL
+    render json: Video.where(where, params[:q])
+  end
 end
--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -4,12 +4,34 @@ class VideosControllerTest < ActionDispatch::IntegrationTest
   test "gets all videos from index" do
     create(:video)
     create(:video)
-    get '/api/v1/library/videos.json'
+    get '/api/v1/library/videos.json' # :format doesn't work in integration tests
     assert_response :success
     assert_equal 2, JSON.parse(@response.body).length
   end
 
-  test "finds videos via simple search api" do
-    # TODO: next up
+  test "finds a videos via simple search api" do
+    create(:video)
+    create(:video, description: "Something about Anatha Pindaka")
+    get videos_search_v1_url, params: { q: "Anath" }
+    assert_response :success
+    assert_equal 1, JSON.parse(@response.body).length
+  end
+
+  test "finds two videos by name and description via search api" do
+    create(:video)
+    create(:video, name: "Anatha Pindaka's Story")
+    create(:video, description: "Something about Anatha Pindaka")
+    get videos_search_v1_url, params: { q: "Anath" }
+    assert_response :success
+    assert_equal 2, JSON.parse(@response.body).length
+  end
+
+  test "search api ignores case" do
+    create(:video)
+    create(:video, name: "Anatha Pindaka's Story")
+    create(:video, description: "Something about Anatha Pindaka")
+    get videos_search_v1_url, params: { q: "anath" }
+    assert_response :success
+    assert_equal 2, JSON.parse(@response.body).length
   end
 end
